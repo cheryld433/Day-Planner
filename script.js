a86b0c5bf1e0dae80db410715547378b93ee2bec
Cheryl Daniels <cheryld433@gmail.com>
2020-04-06 22:53:06 -0400

    updated Javascript

--- script.js
+++ script.js
@@ -1,54 +1,59 @@
+//<----------------------------------CLEAN UP ALL THE NOTES AND VALIDATE THE WORK.-------------------------------------->
+//<----- use class instead id to identify the row ( possible ) an possibly the textarea as well.---->
+
+// Variables:
+let m = moment();
+
+// Date to be displayed in the  heading:
+console.log(m.format('MMMM Do YYYY, h:mm:ss a'));
+$("#currentDay").text(m.format('MMMM Do YYYY, h:mm:ss a'));
+
 $(document).ready(function() {
-  // declare variables
-var currentDay;
-var calendarContainer;
-var time;
-var calendarEvent;
-var saveBtn;
-
-$("#currentDay").text(m.format("dddd, MMMM Do"));
-
-//ready function;
-$(document).ready(function(){
-   //local storage 
-})
-
-// current day displayed in the header
-const getDateAndTime = () => {
-   let dateAndTime = moment().format("dddd, MMMM Do YYYY, h:mm a");
-   dateElement.innerHTML = dateAndTime;
- }
- 
-
-/*
-function realTime() {
-   var dateTime = moment().format("LLLL");
-   console.log(dateTime);
-   $("#currentDay").append(dateTime);
-}
- realTime();
- console.log(realTime);
- 
- var timeHour;
- var hour;
-
-
-
-//scroll down bar on side.
-
-
-//timeblock for standard business hours
-   //block for each time 9am -5pm (9,10,11,12,1,2,3,4,5)
-   /*color code time blocks as followed:
-   time past as grey
-   present time as red
-   future time as green */
-
-/* user must be able to do the following in each time block:
-    enter an event into the timeblock
-    save the event in  local storage*/
-
-
-    // when page is refreshed the event must be saved. 
+  changeColor();
+  getText();
 });
+
+// color code time blocks
+// need to change depending on the time of day
+// may use if/ else statement.( if current hour, else current hour +1, else past hour )
+
+function changeColor() {
+   var realTime = moment().hours();
+    // console.log("Time test" + timeTest);
+    console.log("Current Time" + realTime);
+
+
+
+     // 1. btn click:
+   $('.saveBtn').on('click', function(){
+      var value = $(this).siblings(".note").val();
+
+     //2 get the text value
+     //3- save that to local storage
+     //4- track time, and change colors  
+   });  
+     
+
+
+}
+
+function getText() {
+
+
+}
+
+
+
+//refreshing page
+
+//1 get  value from localstorage
+//2-show that value to the corp text box
+
+
+
+
+
+
+
+ 
 
